939b9281433ba34d0550e43b627fb3caa1972d5f
Your Name <you@example.com>
2024-02-07 13:52:29 +0530

    Create a SpringBoot Application

---
+++ Verlynk-blog-application/src/main/java/com/example/Verlynkblogapplication/VerlynkBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.Verlynkblogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerlynkBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerlynkBlogApplication.class, args);
+	}
+
+}
---
+++ Verlynk-blog-application/src/test/java/com/example/Verlynkblogapplication/VerlynkBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Verlynkblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerlynkBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
